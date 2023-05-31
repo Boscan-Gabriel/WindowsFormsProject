6e968ad32c2b2d5e9883c971c53594d5e88e2148
Boscan-Gabriel <boscan1098@gmail.com>
2023-05-31 18:38:23 +0300

    Deleted image

--- ProiectPaw/Properties/Resources.Designer.cs
+++ ProiectPaw/Properties/Resources.Designer.cs
@@ -59,25 +59,5 @@ namespace ProiectPaw.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap backgoundForm {
-            get {
-                object obj = ResourceManager.GetObject("backgoundForm", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap backgoundForm1 {
-            get {
-                object obj = ResourceManager.GetObject("backgoundForm1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
